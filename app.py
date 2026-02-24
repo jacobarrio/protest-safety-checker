b11e793ffc469c51248cefbd3bdfd2d547584a09
Jacob Lee <jacob61185@gmail.com>
2026-02-23 16:54:27 -0800

    Fix health endpoint and Railway PORT compatibility

--- app.py
+++ app.py
@@ -74,7 +74,7 @@ def api_timeline():
 
 @app.route('/health', methods=['GET'])
 def health():
-return {'ok': True}, 200
+    return {'ok': True}, 200
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
